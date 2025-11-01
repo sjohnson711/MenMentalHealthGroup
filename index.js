2215d48e92592676e59f7afa6967a385743a0ac9
Seth <samaritanbrotherseth@gmail.com>
2025-11-01 07:34:53 -0400

    added the javaSCript to the page

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const testimonies = document.getElementById("testimonies");
 
+const newCard = document.createElement("div");
+newCard.setAttribute("id", "card");
+newCard.innerHTML = `
+    <h1>~Johnon</h1>
+    <p>I have been with these guys for a while and I am thankful to meet them</p>
+`;
 
+testimonies.appendChild(newCard);
 
+const newCard1 = document.createElement("div");
+newCard1.setAttribute("id", "card");
+newCard1.innerHTML = `
+    <h1>~Keisha</h1>
+    <p>Thankful for the times that Jaylon spent time with Mr. Seth and the tutoring and fatherhood he provided Jaylon with. I would recommend this team of professionals a 10 out of 10!
+`;
+testimonies.appendChild(newCard1);
